d3020f3317d3113bc8641c2e3cf5334f2cb6bea8
Yusi-Tau <sf5386@gmail.com>
2025-08-05 03:57:18 +0900

    すべてのファイルの追加

---
+++ src/test/java/firstapp/studentManagement/StudentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package firstapp.studentManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
