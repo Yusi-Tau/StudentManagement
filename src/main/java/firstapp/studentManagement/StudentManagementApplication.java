86d2737de6ce1e2e00f72e1b953907bf1f0398db
Yusi-Tau <sf5386@gmail.com>
2025-08-09 10:22:01 +0900

    workブランチの追加

--- src/main/java/firstapp/studentManagement/StudentManagementApplication.java
+++ src/main/java/firstapp/studentManagement/StudentManagementApplication.java
@@ -1,20 +1,45 @@
 package firstapp.studentManagement;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
 
+	private String name = "Yusi Tau";
+	private String age = "45";
+
+	private Map<String,String> student = new HashMap<>();
+
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String hello() {
-		return "Hello World";
+	@GetMapping("/studentInfo")
+	public Map<String,String> getInfo() {
+		return student;
+	}
+
+	@PostMapping("/studentInfo")
+	public void setInfo(String name, String age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	@PostMapping("/studentName")
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@PostMapping("/studentMap")
+	public void setMap(@RequestParam String name, @RequestParam String age) {
+		this.student.put(name,age);
 	}
 }
