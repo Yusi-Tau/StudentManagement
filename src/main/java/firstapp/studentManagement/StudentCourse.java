c7182493138ac5e6e5cf1ac32460a0c8829876d2
Yusi-Tau <sf5386@gmail.com>
2025-08-11 18:35:32 +0900

    students_coursesの全件取得機能の追加

---
+++ src/main/java/firstapp/studentManagement/StudentCourse.java
@@ -0,0 +1,18 @@
+package firstapp.studentManagement;
+
+import java.time.LocalDate;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class StudentCourse {
+
+  private String id;
+  private String studentId;
+  private String courseName;
+  private LocalDate startDate;
+  private LocalDate completionDate;
+
+}
--- src/main/java/firstapp/studentManagement/StudentManagementApplication.java
+++ src/main/java/firstapp/studentManagement/StudentManagementApplication.java
@@ -5,27 +5,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
 
-	@Autowired
-	private StudentRepository repository;
+  @Autowired
+  private StudentRepository repository;
+
+  @Autowired
+  private StudentCourseRepository courseRepository;
 
 
-	public static void main(String[] args) {
-		SpringApplication.run(StudentManagementApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(StudentManagementApplication.class, args);
+  }
 
-	@GetMapping("/studentList")
-	public List<Student> getStudentList() {
-		return repository.find();
-	}
+  @GetMapping("/studentList")
+  public List<Student> getStudentList() {
+    return repository.search();
+  }
 
-
+  @GetMapping("/studentCourseList")
+  public List<StudentCourse> getStudentCourseList() {
+    return courseRepository.courseSearch();
+  }
 
 
 }
