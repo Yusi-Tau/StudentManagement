4035668cd27e3a65641770dc79627346d01d7960
Yusi-Tau <sf5386@gmail.com>
2025-08-15 12:46:59 +0900

    タイムリーフを使ったReadの画面描画処理

--- src/main/java/firstapp/studentManagement/controller/StudentController.java
+++ src/main/java/firstapp/studentManagement/controller/StudentController.java
@@ -3,14 +3,14 @@ package firstapp.studentManagement.controller;
 import firstapp.studentManagement.controller.converter.StudentConverter;
 import firstapp.studentManagement.data.Student;
 import firstapp.studentManagement.data.StudentsCourses;
-import firstapp.studentManagement.domain.StudentDetail;
 import firstapp.studentManagement.service.StudentService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 
 public class StudentController {
 
@@ -24,16 +24,17 @@ public class StudentController {
   }
 
   @GetMapping("/studentList")
-  public List<StudentDetail> getStudentList() {
+  public String getStudentList(Model model) {
     List<Student> students = service.searchStudentList();
-    List<StudentsCourses> studentsCourses = service.searchStudentsCoursesList();
+    List<StudentsCourses> studentsCourses = service.searchStudentsCourseList();
 
-    return converter.convertStudentDetails(students, studentsCourses);
+    model.addAttribute("studentList", converter.convertStudentDetails(students, studentsCourses));
+    return "studentList";
   }
 
   @GetMapping("/studentsCoursesList")
   public List<StudentsCourses> getStudentCourseList() {
-    return service.searchStudentsCoursesList();
+    return service.searchStudentsCourseList();
   }
 
 }
