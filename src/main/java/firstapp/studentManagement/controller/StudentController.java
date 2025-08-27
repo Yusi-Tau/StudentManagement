a6c7e5f3e521f29f82eb5f1731b4e73368e675b2
Yusi-Tau <sf5386@gmail.com>
2025-08-28 01:25:11 +0900

    SQL文のXMLへの移行

--- src/main/java/firstapp/studentManagement/controller/StudentController.java
+++ src/main/java/firstapp/studentManagement/controller/StudentController.java
@@ -8,6 +8,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * 受講生の検索や登録、更新などを行うREST APIとして実行されるControllerです。
  */
+@Validated
 @RestController
 
 public class StudentController {
