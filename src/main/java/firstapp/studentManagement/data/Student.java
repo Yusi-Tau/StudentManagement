c48e668b7b50c3e6ff0254f39b7be5889a22368d
Yusi-Tau <sf5386@gmail.com>
2025-09-04 00:31:40 +0900

    ドキュメントの作成

--- src/main/java/firstapp/studentManagement/data/Student.java
+++ src/main/java/firstapp/studentManagement/data/Student.java
@@ -1,5 +1,6 @@
 package firstapp.studentManagement.data;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -7,6 +8,7 @@ import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
+@Schema(description = "受講生")
 @Getter
 @Setter
 
